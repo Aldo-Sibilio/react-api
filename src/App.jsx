b44fadc7309f92db2a18bbbd0e9e00760e3b26bc
Aldo Sibilio <sibilioaldo6@gmail.com>
2026-05-07 16:14:20 +0200

    aggiunto fetch degli attori con useEffect e salvataggio nello stato

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,71 @@ import { useState, useEffect } from "react";
 import ActorCard from "./components/ActorCard.jsx";
 
 function App() {
-  return (
-    <div>App</div>
-  )
-}
+
+  // stato per salvare la lista degli attori
+  const [actors, setActors] = useState([]);
+
+  // useEffect per richiedere dati al caricamento della pagina
+  useEffect(() => {
+
+    fetch("https://lanciweb.github.io/demo/api/actors/")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Errore nella risposta del server");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        //stampa in console i dati ricevuti
+        console.log("Dati ricevuti:", data);
+        // salviamo i dati nello stato
+        setActors(data);
+      })
+      .catch((error) => {
+        console.error("Errore durante il caricamento:", error);
+      });
+
+  }, []); // array vuoto, viene eseguito solo al caricamento della pagina
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 export default App
